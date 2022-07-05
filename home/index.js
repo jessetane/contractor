6f89052ad01639fc40bcc36fc63a9b360e6f44f0
Jesse Tane <jesse.tane@gmail.com>
2022-07-05 14:32:27 +0000

    home

--- home/index.js
+++ home/index.js
@@ -5,7 +5,7 @@ import '../contracts-recent/index.js'
 const template = document.createElement('template')
 template.innerHTML = `<h1>Contractor</h1>
 <h2>About</h2>
-<div>This tool can automatically generate a user interface for any contract you have an ABI for. Click <a href=/contracts>here</a> to lookup a contract by its address or ENS name.
+<div id=content>This tool can automatically generate a user interface for any contract you have an ABI for. Click <a href=/contracts>here</a> to lookup a contract by its address or ENS name. The source code can be found at <a href=https://github.com/jessetane/contractor>https://github.com/jessetane/contractor</a>.
 </div>
 <x-contracts-recent></x-contracts-recent>`
 
